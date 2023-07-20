d45700bfb4647f4978380cf8c3b1b76223427c0c
unorji kingsley <unorjikingsley@gmail.com>
2023-07-20 18:15:38 +0100

    solced

--- decoder.rb
+++ decoder.rb
@@ -1,45 +1,43 @@
-class MorseCodeDecoder
-  MORSE_CODE = {
-    '.-' => 'A',
-    '-...' => 'B',
-    '-.-.' => 'C',
-    '-..' => 'D',
-    '.' => 'E',
-    '..-.' => 'F',
-    '--.' => 'G',
-    '....' => 'H',
-    '..' => 'I',
-    '.---' => 'J',
-    '-.-' => 'K',
-    '.-..' => 'L',
-    '--' => 'M',
-    '-.' => 'N',
-    '---' => 'O',
-    '.--.' => 'P',
-    '--.-' => 'Q',
-    '.-.' => 'R',
-    '...' => 'S',
-    '-' => 'T',
-    '..-' => 'U',
-    '...-' => 'V',
-    '.--' => 'W',
-    '-..-' => 'X',
-    '-.--' => 'Y',
-    '--..' => 'Z'
-  }.freeze
-
-  def self.decode_char(morse_char)
-    MORSE_CODE[morse_char]
-  end
-
-  def self.decode_word(morse_word)
-    morse_word.split.map { |morse_char| decode_char(morse_char) }.join
-  end
-
-  def self.decode(message)
-    message.split('   ').map { |morse_word| decode_word(morse_word) }.join(' ')
-  end
+def decode_char(morse_code)
+  morse_letters = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C',
+    '-..' => 'D', '.'     => 'E', '..-.'  => 'F',
+    '--.' => 'G', '....'  => 'H', '..'    => 'I',
+    '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+    '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R',
+    '...' => 'S', '-' => 'T', '..-' => 'U',
+    '...-' => 'V', '.--'   => 'W', '-..-' => 'X',
+    '-.--' => 'Y', '--..'  => 'Z'
+  }
+  morse_letters[morse_code]
 end
 
-puts MorseCodeDecoder.decode('-- -.--   -. .- -- .')
-puts MorseCodeDecoder.decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+def decode_word(morse_code)
+  decoded_word = ''
+  chars = morse_code.split
+  chars.each do |char|
+    decoded_word += decode_char(char)
+  end
+  decoded_word
+end
+
+def decode_message(morse_code)
+  decoded_message = ''
+  words = morse_code.split('   ')
+  words.each do |word|
+    decoded_message += "#{decode_word(word)} "
+  end
+  decoded_message
+end
+
+# Example usage
+char = decode_char('.-')
+puts char
+
+word = decode_word('-- -.--')
+puts word
+
+message = decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..
+  --- ..-.   .-. ..- -... .. . ...')
+puts message
